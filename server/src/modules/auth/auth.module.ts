87236ada8bfe81d10ccc4228150ab51c41286089
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-19 14:09:05 +0100

    feat(auth module): create AuthModule with JWT and Passport integration

---
+++ server/src/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User, UserSchema } from '../users/schemas/user.schema';
+import { JwtStrategy } from './strategies/jwt.stategy';
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, RefreshTokenStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
